7d83c69a2c2b8b02e4cfe052eb8cd0d4beab017a
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-26 22:31:17 -0300

    feat: Admin repository
    Also create a seeder for tests admin data

---
+++ src/data/repositories/admin-repository.ts
@@ -0,0 +1,39 @@
+import { eq } from 'drizzle-orm'
+
+import { AdminEntity } from '@/domain/entities/admin-entity'
+
+import { db } from '../db'
+import { admin } from '../db/schema'
+
+interface IAdminRepository {
+  login: (credentials: {
+    email: string
+    password: string
+  }) => Promise<{ admin: AdminEntity }>
+}
+
+export class AdminRepository implements IAdminRepository {
+  async login(credentials: {
+    email: string
+    password: string
+  }): Promise<{ admin: AdminEntity }> {
+    const { email, password } = credentials
+
+    const [adminUser] = await db
+      .select()
+      .from(admin)
+      .where(eq(admin.email, email))
+
+    if (!adminUser) {
+      throw new Error('Invalid email')
+    }
+
+    if (adminUser.password === password) {
+      return {
+        admin: new AdminEntity(adminUser),
+      }
+    }
+
+    throw new Error('Invalid password')
+  }
+}
